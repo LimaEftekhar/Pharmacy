eb969d826a3d4457a661b27031405df6b23dc71d
Lima Eftekhar <limaeftkhar@gmail.com>
2026-04-04 22:25:57 -0500

    Removed deprecated options: {       useNewUrlParser: true,       useUnifiedTopology: true,     }

--- db/connect.js
+++ db/connect.js
@@ -10,10 +10,7 @@ mongoose.set("bufferCommands", false);
 //Start: Recently added
 const connectDB = async (url) => {
   try {
-    const conn = await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(url);
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
